e5ffd537a89aa92a4ab54cb69a8f937352500260
tamitavares <tamiris.lemes18@gmail.com>
2021-11-22 18:28:54 -0300

    Finalizado com sucesso

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
 function alertar(){
     alert("Vocâ será redirecionado ao WhatsApp da empresa! Faça seu pedido!");
-}
+}//alertar
 
 function soma(){
     var selecionados = document.getElementsByName('OptSorvete');
     for (var i = 0; i < selecionados.length; i++){
         if (selecionados[i].checked) {
-            if(selecionados[i].value == "Morango"){
+            if(selecionados[i].id == "opt-1"){
                 document.getElementById("txt").innerHTML= "Pote de Morango";
-            }else if (selecionados[i].value == "Chocolate") {
+            }else if (selecionados[i].id == "opt-2") {
                 document.getElementById("txt").innerHTML= "Pote de Chocolate";
-            }else if (selecionados[i].value == "Creme") {
+            }else if (selecionados[i].id == "opt-3") {
                 document.getElementById("txt").innerHTML= "Pote de Creme";
             }
-        }
-    }
-}
-
-
-function mostrar(){
-    var selecionados = document.getElementsByName('OptSorvete');
-    for (var i = 0; i < selecionados.length; i++){
-        if (selecionados[i].checked) {
-            if(selecionados[i].value == Opt1){
-                document.getElementById("txt").innerHTML= "Morango";
-            }else if (selecionados[i].value == "Chocolate") {
-                var Opt2 = "Chocolate";
-                alert(Opt2);
-            }else if (selecionados[i].value == "Creme") {
-                var Opt3 = "Creme";
-                alert(Opt3);
+            else if (selecionados[i].id == "opt-4") {
+                document.getElementById("txt").innerHTML= "Picolé de Morango";
+            }
+            else if (selecionados[i].id == "opt-5") {
+                document.getElementById("txt").innerHTML= "Picolé de Limão";
+            }
+            else if (selecionados[i].id == "opt-6") {
+                document.getElementById("txt").innerHTML= "Picolé de Maracujá";
+            }
+            else if (selecionados[i].id == "opt-7") {
+                document.getElementById("txt").innerHTML= "Zero Lactose de Mirtilo";
+            }
+            else if (selecionados[i].id == "opt-8") {
+                document.getElementById("txt").innerHTML= "Zero Lactose de Maracujá";
+            }
+            else if (selecionados[i].id == "opt-9") {
+                document.getElementById("txt").innerHTML= "Zero Lactose de Amora";
+            }
+            else if (selecionados[i].id == "opt-10") {
+                document.getElementById("txt").innerHTML= "Pote Especial de Frutas Vermelhas";
+            }
+            else if (selecionados[i].id == "opt-11") {
+                document.getElementById("txt").innerHTML= "Pote Especial de Bombom";
+            }
+            else if (selecionados[i].id == "opt-12") {
+                document.getElementById("txt").innerHTML= "Picolé Especial de Uva Suíça";
+            }
+            else if (selecionados[i].id == "opt-13") {
+                document.getElementById("txt").innerHTML= "Picolé Especial de Bombom Recheado";
+            }
+            else if (selecionados[i].id == "opt-14") {
+                document.getElementById("txt").innerHTML= "Zero Lactose Especial de Napolitano";
+            }
+            else if (selecionados[i].id == "opt-15") {
+                document.getElementById("txt").innerHTML= "Zero Lactose Especial de Frutas Vermelhas em Pedaços";
             }
         }
     }
-    
-}
+}//soma
